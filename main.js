9cc61c74cad7272e06af524a5783febef561ed4d
Paviterjeet <paviterjeetkaur@gmail.com>
2025-03-22 22:38:23 +0530

    all blog section done

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import e from "express";
 import signupRouter from "./routes/signup.js"
 import loginRouter from "./routes/login.js" 
-import userRouter from "./routes/user.js" 
+import userRouter from "./routes/user.js"
+import blogRouter from "./routes/blogs.js" 
 
 const port = 3000
 const app = e()
@@ -13,9 +14,10 @@ app.use(e.static('public'))
 app.use("/signup",signupRouter)
 app.use("/login",loginRouter)
 app.use("/user",userRouter)
+app.use("/blogs",blogRouter)
 
 app.get("/",(req, res)=>{
-    res.render('userProfile')
+    res.render('index')
 })
 
 
