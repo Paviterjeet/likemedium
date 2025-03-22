008d6ac2d0c7e4d9a89ea40829ccc321215ed7a9
Paviterjeet <paviterjeetkaur@gmail.com>
2025-03-22 16:45:17 +0530

    public and views added

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ import e from "express";
 const port = 3000
 const app = e()
 
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+app.use(e.static('public'));
+
 app.get("/",(req, res)=>{
-    res.send("<h1>Hello World !<h1>")
+    res.render('index')
 })
+
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`)
