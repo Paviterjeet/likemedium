14052d3792785a0ae568d75381dd72937f17e538
Paviterjeet <paviterjeetkaur@gmail.com>
2025-03-22 18:25:14 +0530

    signin and login route added

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 import e from "express";
+import signupRouter from "./routes/signup.js"
+import loginRouter from "./routes/login.js" 
 
 const port = 3000
 const app = e()
 
 // Set EJS as view engine
-app.set('view engine', 'ejs');
-app.use(e.static('public'));
+app.set('view engine', 'ejs')
+app.use(e.static('public'))
+
+app.use("/signup",signupRouter)
+app.use("/login",loginRouter)
 
 app.get("/",(req, res)=>{
     res.render('index')
