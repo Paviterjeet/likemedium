22d36861fbb8a8a52489c64842337e581da9b95d
Paviterjeet <paviterjeetkaur@gmail.com>
2025-03-23 14:16:40 +0530

    addblog forum added

--- routes/blogs.js
+++ routes/blogs.js
@@ -4,5 +4,9 @@ const router = e.Router()
 router.get('/',(req,res)=>{
     res.render('allBlogs')
 })
+router.get('/add',(req,res)=>{
+   // res.render('likeMedium')
+    res.render('addBlog')
+})
 
 export default router
